5c37155682d9b893e4ee023f98516c3a8f31abf5
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-04-20 20:36:07 -0300

    Implementing the timer and improving the project organization

---
+++ js/customJqueryFunctions.js
@@ -0,0 +1,8 @@
+$.fn.timer = function () {
+    let timerSeconds = 30;
+    const timer = setInterval(function () {
+        $('.timer').html(timerSeconds-- + ' seconds');
+        if (timerSeconds < 0)
+            clearInterval(timer);
+    }, 1000);
+}
