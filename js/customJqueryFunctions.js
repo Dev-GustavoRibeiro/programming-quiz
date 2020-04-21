a557538b3a6530a7ffeb5455f9ea1d43b4db60e5
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-04-21 14:16:07 -0300

    Implementing the logic to exchange questions

--- js/customJqueryFunctions.js
+++ js/customJqueryFunctions.js
@@ -1,7 +1,7 @@
 $.fn.timer = function () {
     let timerSeconds = 30;
     const timer = setInterval(function () {
-        $('.timer').html(timerSeconds-- + ' seconds');
+        $('.timer').html('<i class="far fa-clock"></i> ' + timerSeconds-- + ' seconds');
         if (timerSeconds < 0)
             clearInterval(timer);
     }, 1000);
--- js/quiz.js
+++ js/quiz.js
@@ -10,19 +10,30 @@ $('.start').click(function () {
     timer = $().timer();
 });
 
-const pressNextCss = { 'box-shadow': 'none', 'top': '19px' };
-const nextButtonCssBackup = { 'box-shadow': '0px 4px 0px 0px rgb(96, 59, 163)', 'top': '15px' };
+const pressNextCss = { 'box-shadow': 'none', 'top': '4px' };
+const nextButtonCssBackup = { 'box-shadow': '0px 4px 0px 0px rgb(96, 59, 163)', 'top': '0px' };
 const questions = [
     '2. Which of these expressions is NOT a valid way to add 1 to a variable in JavaScript?',
     '3. Katherine is creating a computer program that will allow the cursor to jump to a different location when the mouse is clicked on a certain image. Which programming component should Katherine use to tell the program what to do when the mouse is clicked on the image?',
     '4. What is the only language that a computer can understand?',
     '5. Which term is used to describe expressions that result in the value of either true or false?'
 ]
+const choices = [
+    ['x++', 'x = x + 1', 'x += 1', 'x+'],
+    ['a parameter', 'An event', 'A class', 'An event handler'],
+    ['High-level', 'Machine', 'Application', 'Assembly'],
+    ['Selection', 'Conditional', 'Boolean', 'Concatenation']
+]
 let questionCounter = 2;
 
 $('.next-button').mousedown(function () {
     $(this).css(pressNextCss);
-    $('.question-number').html('Question ' + questionCounter++);
+    $('.question-number').html('<i class="far fa-dot-circle"></i>' + ' Question ' + questionCounter++);
+    $('.question').hide().html(questions[questionCounter - 3]).fadeIn(500);
+    if (questionCounter === 6) $('.next-button').css('display', 'none');
+    $('[option]').each(function(index) {
+        $(this).hide().html('<button class="choice"></button>' + choices[questionCounter - 3][index]).fadeIn(500);
+    });
     clearInterval(timer);
     timer = $().timer();
 }).mouseup(function () {
